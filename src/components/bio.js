74e36bf6ed10e24fd605adf74b2a7fd67148ab5a
James <contact@james-wright.co.uk>
2020-04-05 13:21:20 +0100

    resolves mc

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,69 +1,57 @@
-import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import Image from 'gatsby-image';
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Image from "gatsby-image";
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile.jpeg/" }) {
-        childImageSharp {
-          fixed(width: 100, height: 100, quality: 100) {
-            ...GatsbyImageSharpFixed
+      bio: file(sourceInstanceName: { eq: "bio" }) {
+        id
+        childMarkdownRemark {
+          headings {
+            value
+            depth
           }
-        }
-      }
-      linkedin: file(absolutePath: { regex: "/linkedin.png/" }) {
-        childImageSharp {
-          fixed(width: 25, height: 25) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      site {
-        siteMetadata {
-          author
-          social {
-            twitter
+          frontmatter {
+            name
+            description
+            linkedin
           }
         }
       }
     }
   `);
 
-  const { author, social } = data.site.siteMetadata;
+  const bio = data.bio.childMarkdownRemark.frontmatter;
+
   return (
     <div className="c-bio">
-      <Image
+      {/* <Image
         className="c-bio__image"
         fadeIn
         fixed={data.avatar.childImageSharp.fixed}
         objectFit="cover"
         objectPosition="50% 50%"
         alt=""
-      />
+      /> */}
       <p>
-        Written by
-        {' '}
-        <strong>{author}</strong>
-        {' '}
-        who lives and works in San
-        Francisco building useful things.
-        {' '}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
+        {bio.description}
       </p>
       <div className="c-bio__social">
-        <a href="https://uk.linkedin.com/in/fiona-points" target="_blank" rel="noopener noreferrer">
-          <Image
+        <a
+          href={bio.linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {/* <Image
             fadeIn
             fixed={data.linkedin.childImageSharp.fixed}
             objectFit="cover"
             objectPosition="50% 50%"
             alt=""
-          />
+          /> */}
         </a>
-      </div>
+    </div>
     </div>
   );
 };
